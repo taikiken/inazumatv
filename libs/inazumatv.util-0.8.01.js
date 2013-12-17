2bbd714b1299d05b4998ca1c9535fcba7251a603
taikiken <denkosekka@gmail.com>
2013-12-17 18:50:29 +0900

    ライセンス修正

--- libs/inazumatv.util-0.8.01.js
+++ libs/inazumatv.util-0.8.01.js
@@ -2215,6 +2215,34 @@ var inazumatv = {};
  *
  * This notice shall be included in all copies or substantial portions of the Software.
  */
+/**
+ * ShuffleText by Yasunobu Ikeda. Feb 3, 2012
+ * Visit http://clockmaker.jp/ for documentation, updates and examples.
+ *
+ *
+ * Copyright (c) 2012 Yasunobu Ikeda
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 // -----------------------------------
 //  original code by clockMaker's  ShuffleText.js
 //  http://clockmaker.jp/blog/2012/02/html5_shuffletext/
@@ -2762,7 +2790,7 @@ var inazumatv = {};
  *
  * This notice shall be included in all copies or substantial portions of the Software.
  */
-/*!
+/**
  * Smooth Scroll - v1.4.13 - 2013-11-02
  * https://github.com/kswedberg/jquery-smooth-scroll
  * Copyright (c) 2013 Karl Swedberg
@@ -2869,12 +2897,14 @@ var inazumatv = {};
 
         $.fn.extend({
             scrollable: function(dir) {
-                var scrl = getScrollable.call(this, {dir: dir});
-                return this.pushStack(scrl);
+//                var scrl = getScrollable.call(this, {dir: dir});
+//                return this.pushStack(scrl);
+                return this.pushStack(getScrollable.call(this, {dir: dir}));
             },
             firstScrollable: function(dir) {
-                var scrl = getScrollable.call(this, {el: 'first', dir: dir});
-                return this.pushStack(scrl);
+//                var scrl = getScrollable.call(this, {el: 'first', dir: dir});
+//                return this.pushStack(scrl);
+                return this.pushStack(getScrollable.call(this, {el: 'first', dir: dir}));
             },
 
             smoothScroll: function(options, extra) {
